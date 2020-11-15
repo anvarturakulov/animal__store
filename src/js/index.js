584692beb5e34c2455dbcb5a7af651dcc2af556e
Anvar <anvarsam@mail.ru>
2020-11-15 14:50:29 +0500

    html structere and adaptive responce

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,19 @@
 // Main js file
 
-console.log('its work');
+"use strict";
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    let menuBtn = document.querySelector('.menu__btn');
+    let menu = document.querySelector('.menu');
+
+
+    menuBtn.addEventListener('click',()=>{
+        if (menu.style.display == 'block') {
+            menu.style.display = 'none';
+        } else {
+            menu.style.display = 'block';
+        }
+
+    });
+
+});
